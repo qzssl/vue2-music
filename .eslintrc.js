353baf148aac470f732e619e397b11f899b5035d
qzssl <2597253199@qq.com>
2019-09-19 20:06:23 +0800

    header

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,9 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //添加规则
+    'eol-last': 0,
+    'space-before-function-paren': 0
   }
 }
---
+++ src/components/rank/rank.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        排名页面
+    </div>
+</template>
+<script>
+export default {
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
